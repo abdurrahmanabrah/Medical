1ab32f91443a8ea7607128b1e1370efc21384bc9
Abdur Rahman Abrah <134528665+abdurrahmanabrah@users.noreply.github.com>
2025-01-30 20:18:15 +0600

    Delete Medical directory

--- Medical/Model/Patient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hospital.Model
-{
-    public class Patient
-    {
-        public int Id { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-    }
-}
